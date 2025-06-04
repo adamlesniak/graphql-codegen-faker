335db7749274efd9841e35d57ef2f084ecd2546c
Adam Lesniak <hello@alesniak.com>
2025-06-04 14:52:42 +0200

    feat(core): Add in ListType support - fakerNested

--- tests/typescript.spec.ts
+++ tests/typescript.spec.ts
@@ -75,4 +75,47 @@ describe('Faker', () => {
       ].join('\n')
     );
   });
+
+
+  it('should generate nested types - array', async () => {
+    const schema = buildSchema(/* GraphQL */ `
+      scalar FakerArgs
+
+      directive @faker(
+        module: String!
+        method: String!
+        args: FakerArgs
+      ) on FIELD_DEFINITION
+      directive @fakerNested on FIELD_DEFINITION
+      directive @fakerList(items: Int!, name: String!) on OBJECT
+
+      type User @fakerList(items: 20, name: user) {
+        id: String @faker(module: string, method: uuid)
+        name: String @faker(module: person, method: firstName)
+        email: String
+        password: String
+        properties: [UserProperties]! @fakerNested
+        createdAt: String
+        updatedAt: String
+      }
+
+      type UserProperties @fakerList(items: 20, name: userProperties) {
+        id: String @faker(module: string, method: uuid)
+        name: String @faker(module: person, method: firstName)
+      }
+    `);
+    const result = await plugin(schema, [], {}, { outputFile: '' });
+
+    expect(result.prepend).toEqual([
+      "import { fakerEN as faker } from '@faker-js/faker';",
+    ]);
+    expect(result.content).toEqual(
+      [
+        'export const mockUser = {id: faker.string.uuid(),name: faker.person.firstName(),properties: [{id: faker.string.uuid(),name: faker.person.firstName()}]};',
+        'export const mockUserList = Array.from({ length: 20 }, () => ({...mockUser}));',
+        'export const mockUserProperties = {id: faker.string.uuid(),name: faker.person.firstName()};',
+        'export const mockUserPropertiesList = Array.from({ length: 20 }, () => ({...mockUserProperties}));',
+      ].join('\n')
+    );
+  });
 });
